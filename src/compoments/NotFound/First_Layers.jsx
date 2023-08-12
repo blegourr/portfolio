6af582ed0ece3e2ac0ef756c7d4f3c163d4f8158
blegourr <legourrierecbaptiste@gmail.com>
2023-08-12 11:54:18 +0200

    add page 404

--- src/compoments/NotFound/First_Layers.jsx
+++ src/compoments/NotFound/First_Layers.jsx
@@ -4,7 +4,9 @@ import './First_Layers.css';
 function FirstLayers() {
   return (
     <div className="containerFirstLayer404">
-      
+        <span>4</span>
+        <span>0</span>
+        <span>4</span>
     </div>
   );
 }
