af5cae08a11fce14766fccbd923955114a752c79
Blegourr <91756178+blegourr@users.noreply.github.com>
2023-11-20 10:46:14 +0000

    fin cours

--- src/fixed/header/Header.jsx
+++ src/fixed/header/Header.jsx
@@ -1,9 +1,32 @@
 import { useNavigate } from 'react-router-dom';
 import './Header.css'
 import Blegourr from '../../compoments/assets/logo/Blegourr.webp'
+import { useRef } from 'react';
 
 function Header() {
   const nav = useNavigate();
+
+  // détecte quand l'utilisateur scroll si supèrieur à 0 ajouter la class scroll
+  const refBackground = useRef(0);
+
+  const handleNavigation = (e) => {
+    const window = e.currentTarget;
+    if (y > window.scrollY) {
+      console.log("scrolling up");
+    } else if (y < window.scrollY) {
+      console.log("scrolling down");
+    }
+    setY(window.scrollY);
+  }; 
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => handleNavigation(e));
+
+    return (() => {
+      window.removeEventListener("scroll", (e) => handleNavigation(e));
+
+    })
+  }, []);
 
 
   return (
